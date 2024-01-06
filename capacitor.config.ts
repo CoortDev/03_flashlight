755ec8cbc61cf74c6bc7d276e86e53b0ae21259c
CoortDev35 <coort.dev.35@gmail.com>
2024-01-06 17:14:27 -0600

    App de linterna, enciende y apaga el led de un dispositivo mobil.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: '03_flashlight',
+  appName: 'Flashlight',
   webDir: 'www',
   server: {
     androidScheme: 'https'
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Flashlight } from '@awesome-cordova-plugins/flashlight/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  isActive: boolean = false;
 
+  constructor(private flashlight: Flashlight) {}
+
+  flash() {
+    if (this.isActive) {
+      this.flashlight.switchOn()
+    }
+    else {
+      this.flashlight.switchOff()
+    }
+  }
 }
